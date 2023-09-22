60a696d958b7cd9db468a8a648a707a90efe60e6
timontuitoek <tuitoektimon@gmail.com>
2023-09-22 12:44:17 -0700

    changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
 /**
@@ -15,7 +13,7 @@ void bubble_sort(int *array, size_t size)
         size_t i, j;
         int temp, swapped;
 
-        if (array == NULL || size == 0)
+        if (array == NULL || size < 2)
                 return;
 
         for (i = 0; i < size - 1; i++)
