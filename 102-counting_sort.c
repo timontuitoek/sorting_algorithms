0609264905046443aec54fd6aecfcea83181eafd
timontuitoek <tuitoektimon@gmail.com>
2023-09-26 01:26:00 -0700

    A function that sorts an array of integers in ascending order using the Heap sort algorithm

---
+++ 102-counting_sort.c
@@ -0,0 +1,55 @@
+#include "sort.h"
+
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ *	using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *count, *sorted, max, u;
+
+	if (array == NULL || size < 2)
+	return;
+
+	sorted = malloc(sizeof(int) * size);
+	if (sorted == NULL)
+	return;
+
+	/* Find the maximum value in the array */
+	max = array[0];
+	for (u = 1; u < (int)size; u++)
+	{
+		if (array[u] > max)
+		max = array[u];
+	}
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
+	{
+		free(sorted);
+		return;
+	}
+	for (u = 0; u <= max; u++)
+	count[u] = 0;
+
+	for (u = 0; u < (int)size; u++)
+	count[array[u]] += 1;
+
+	print_array(count, max + 1);
+
+	for (u = 0; u < (int)size; u++)
+	{
+		sorted[count[array[u]] - 1] = array[u];
+		count[array[u]] -= 1;
+	}
+
+	for (u = 0; u < (int)size; u++)
+	array[u] = sorted[u];
+
+	free(sorted);
+	free(count);
+}
+
---
+++ 104-heap_sort.c
@@ -0,0 +1,67 @@
+#include "sort.h"
+
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+void heap_sort(int *array, size_t size);
+
+/**
+ * max_heapify - Maintain the max heap property.
+ * @array: The array to be heapified.
+ * @size: The size of the array.
+ * @base: The base index.
+ * @root: The root index.
+ */
+void max_heapify(int *array, size_t size, size_t base, size_t root)
+{
+	size_t left, right, largest;
+
+	left = 2 * root + 1;
+	right = 2 * root + 2;
+	largest = root;
+
+	if (left < base && array[left] > array[largest])
+	largest = left;
+
+	if (right < base && array[right] > array[largest])
+	largest = right;
+
+	if (largest != root)
+	{
+	/* Swap elements directly without a separate swap function*/
+	int tmp = array[root];
+
+	array[root] = array[largest];
+	array[largest] = tmp;
+
+	print_array(array, size);
+	max_heapify(array, size, base, largest);
+	}
+}
+
+/**
+ * heap_sort - Sort an array of integers using heap sort.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void heap_sort(int *array, size_t size)
+{
+	int i;
+
+	if (array == NULL || size < 2)
+	return;
+
+	for (i = size / 2 - 1; i >= 0; i--)
+	max_heapify(array, size, size, i);
+
+	for (i = size - 1; i > 0; i--)
+	{
+	/* Swap elements directly without a separate swap function*/
+	int tmp = array[0];
+
+	array[0] = array[i];
+	array[i] = tmp;
+
+	print_array(array, size);
+	max_heapify(array, size, i, 0);
+	}
+}
+
